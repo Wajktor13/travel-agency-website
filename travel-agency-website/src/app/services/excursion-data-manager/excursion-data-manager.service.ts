f21b16836f6b8ea12aac8202fb658b6476c9b75e
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-12-03 23:50:41 +0100

    update

--- travel-agency-website/src/app/services/excursion-data-manager/excursion-data-manager.service.ts
+++ travel-agency-website/src/app/services/excursion-data-manager/excursion-data-manager.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ExcursionData } from '../../shared/models/excursions-data';
 
 
@@ -10,9 +10,20 @@ import { ExcursionData } from '../../shared/models/excursions-data';
 
 export class ExcursionDataManagerService {
   private DATA_URL:string = 'http://localhost:3000/excursion-data'
-  public excursionsData: Observable<ExcursionData[]>
+  private fetchedData: Observable<ExcursionData[]>
+  public excursionsData: BehaviorSubject<ExcursionData[]> = new BehaviorSubject([]as ExcursionData[])
 
   constructor(private httpClient: HttpClient) { 
-    this.excursionsData = httpClient.get<ExcursionData[]>(this.DATA_URL)
+    this.fetchedData = httpClient.get<ExcursionData[]>(this.DATA_URL)
+    this.fetchedData.subscribe(
+      {
+        next: (data: ExcursionData[]) => this.excursionsData.next(data),
+        error: (err: any) => console.log(err)
+      }
+    )
+  }
+
+  setExcursionsData(newExcursionsData: ExcursionData[]){
+    this.excursionsData.next(newExcursionsData)
   }
 }
