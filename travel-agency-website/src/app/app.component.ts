95f39445ea52a4b8db46da0076e62576334890d8
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-12-17 00:20:43 +0100

    update visuals

--- travel-agency-website/src/app/app.component.ts
+++ travel-agency-website/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'travel-agency-website';
   
-  constructor(){}
+  constructor(private router: Router){}
+
+  navigateToHome(): void{
+    this.router.navigate(['home'])
+  }
+
+  navigateToExcursions(): void{
+    this.router.navigate(['excursions'])
+  }
+
+  navigateToAddExcursionForm(): void{
+    this.router.navigate(['add-excursion-form'])
+  }
+
+  navigateToAddReservationsHistory(): void{
+    this.router.navigate(['reservations-history'])
+  }
+
+  navigateToCart(): void{
+    this.router.navigate(['cart'])
+  }
 }
