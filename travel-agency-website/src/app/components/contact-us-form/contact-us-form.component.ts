8b201c09d55385a0f18b675fb6e99be85c84757e
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-08-14 15:24:33 +0200

    add validation to contact form

--- travel-agency-website/src/app/components/contact-us-form/contact-us-form.component.ts
+++ travel-agency-website/src/app/components/contact-us-form/contact-us-form.component.ts
@@ -17,13 +17,48 @@ export class ContactUsFormComponent {
   constructor() { }
 
   public onSubmit(e: Event): void {
-    e.preventDefault();
+    e.preventDefault()
+
+    if (!this.validateForm()) { 
+      return
+    }
 
     emailjs.sendForm('travel-agency-website', 'template_7c8tdt7', e.target as HTMLFormElement, 'wL8YyNQC9vEfsNiD2')
-      .then((result: EmailJSResponseStatus) => {
-        alert('Your message has been sent!');
+      .then((_: EmailJSResponseStatus) => {
+        alert('Your message has been sent!')
       }, (error) => {
-        console.log(error.text);
+        console.log(error.text)
       });
+  }
+
+  private validateForm(): boolean {
+    return this.validateNameSurname(this.name) && this.validateNameSurname(this.surname) && this.validateEmail(this.email) && this.validateMessage(this.message)
+  }
+
+  private validateNameSurname(str: string): boolean {
+    if(/^[a-zA-Z]+$/.test(str) && str.length > 0){
+      return true
+    } else {
+      alert('name and surname must contain only letters and cannot be empty')
+      return false
+    }
+  }
+
+  private validateEmail(email: string): boolean {
+    if(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+      return true
+    } else {
+      alert('invalid email')
+      return false
+    }
+  }
+
+  private validateMessage(message: string): boolean {
+    if(message.length > 0){
+      return true
+    } else {
+      alert('message cannot be empty');
+      return false
+    }
   }
 }
