f06ac8488073832f12ffb72de5c526d122ec685e
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-08-16 23:19:43 +0200

    add password validation

--- travel-agency-website/src/app/components/login-register/login-register.component.ts
+++ travel-agency-website/src/app/components/login-register/login-register.component.ts
@@ -18,6 +18,11 @@ export class LoginRegisterComponent implements OnInit {
   public password: string = ''
   public repeatedPassword: string = ''
 
+  public minCharsRequirement: PasswordRequirement = new PasswordRequirement(8, false, 'min-chars-requirement-img')
+  public minSpecialCharsRequirement: PasswordRequirement = new PasswordRequirement(1, false, 'min-special-chars-requirement-img')
+  public minDigitsRequirement: PasswordRequirement = new PasswordRequirement(1, false, 'min-digits-requirement-img')
+  public minUppercaseCharsRequirement: PasswordRequirement = new PasswordRequirement(1, false, 'min-uppercase-chars-requirement-img')
+
   constructor(
     public authService: AuthService,
     private router: Router
@@ -42,6 +47,14 @@ export class LoginRegisterComponent implements OnInit {
   }
 
   public registerButtonClicked(): void {
+    if (this.password !== this.repeatedPassword) {
+      alert('passwords are not the same')
+      return
+    } else if (this.minCharsRequirement.status === false || this.minSpecialCharsRequirement.status === false || this.minDigitsRequirement.status === false || this.minUppercaseCharsRequirement.status === false) {
+      alert('password does not meet requirements')
+      return
+    }
+    
     this.authService.register(this.email, this.password, this.nickname)
   }
 
@@ -66,5 +79,49 @@ export class LoginRegisterComponent implements OnInit {
         input.type = 'password'
         visibilityToggleImg.src = '../../../assets/images/show_password_icon.png'
       }
+  }
+
+  public checkPasswordRequirements(event: Event): void {
+    let password: string = (event.target as HTMLInputElement).value
+
+    this.minCharsRequirement.status = password.length >= this.minCharsRequirement.required
+    if (this.minCharsRequirement.status) {
+      document.getElementById(this.minCharsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_fulfilled_icon.png')
+    } else {
+      document.getElementById(this.minCharsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_not_fulfilled_icon.png')
+    }
+
+    this.minSpecialCharsRequirement.status = password.match(/[^a-zA-Z0-9]/g)!?.length >= this.minSpecialCharsRequirement.required
+    if (this.minSpecialCharsRequirement.status) {
+      document.getElementById(this.minSpecialCharsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_fulfilled_icon.png')
+    } else {
+      document.getElementById(this.minSpecialCharsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_not_fulfilled_icon.png')
+    }
+
+    this.minDigitsRequirement.status = password.match(/[0-9]/g)!?.length >= this.minDigitsRequirement.required
+    if (this.minDigitsRequirement.status) {
+      document.getElementById(this.minDigitsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_fulfilled_icon.png')
+    } else {
+      document.getElementById(this.minDigitsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_not_fulfilled_icon.png')
+    }
+
+    this.minUppercaseCharsRequirement.status = password.match(/[A-Z]/g)!?.length >= this.minUppercaseCharsRequirement.required
+    if (this.minUppercaseCharsRequirement.status) {
+      document.getElementById(this.minUppercaseCharsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_fulfilled_icon.png')
+    } else {
+      document.getElementById(this.minUppercaseCharsRequirement.statusImgElementID)?.setAttribute('src', '../../../assets/images/requirement_not_fulfilled_icon.png')
+    }
+  }
+}
+
+class PasswordRequirement {
+  public required: number
+  public status: boolean
+  public statusImgElementID: string
+
+  constructor(required: number, status: boolean, statusImgElementID: string) {
+    this.required = required
+    this.status = status
+    this.statusImgElementID = statusImgElementID
   }
 }
